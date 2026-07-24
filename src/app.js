80870047484f748830617707ea31cb8fbe27b41f
aditya <adityashiv005@gmail.com>
2026-07-24 16:32:30 +0530

    added spaces

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 
 
 // routes import
-import userRouter from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js" 
 
 // routes decleration 
 app.use("/api/v1/users", userRouter)
